00c734dc0a29414a636edfce2348b3c86c041e29
ItsyPetkov <44676234+ItsyPetkov@users.noreply.github.com>
2023-03-13 12:15:20 +0000

    Modifications to original DAG-GNN

--- DAG-WGAN/FullDataPreProcessor.py
+++ DAG-WGAN/FullDataPreProcessor.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Nov  2 20:13:45 2020
+"""
 
 """
 Modifications copyright (C) 2021 Hristo Petkov
 This file is not part of the original DAG-GNN code.
 Modifications are as follows:
-  -Addition of a new file containiing pre-processing functionality which affects input data 
+  -Addition of a new file containing pre-processing functionality for the input data
 """
 
+"""
 @inproceedings{xu2019modeling,
   title={Modeling Tabular data using Conditional GAN},
   author={Xu, Lei and Skoularidou, Maria and Cuesta-Infante, Alfredo and Veeramachaneni, Kalyan},
